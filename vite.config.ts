546ff2f214a180be282319ea4c03a075bcabca05
atlastechbrasil <alex@atlastechbrasil.com>
2025-06-04 15:17:59 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
+    allowedHosts: ['n8nalex-dentista.zn6b4j.easypanel.host'],
   },
   plugins: [
     react(),
